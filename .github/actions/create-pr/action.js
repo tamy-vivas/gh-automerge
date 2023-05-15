f766ddf83e5d2a69e5cc9fbeda959e94f14d95c4
tamy-vivas <tamy.vivas@telusinternational.com>
2023-05-15 12:28:38 -0600

    test: 2

--- .github/actions/create-pr/action.js
+++ .github/actions/create-pr/action.js
@@ -1,28 +1,32 @@
-module.exports = async ({ github, context }) => {
-  const { repo, owner } = context.repo;
-  const { data } = await github.rest.pulls.create({
-    title: "[NEW PR] Sync main",
-    owner,
-    repo,
-    head: "sit",
-    base: "main",
-    body: [
-      "This PR is auto-generated",
-      "[Full history of actions](https://github.com/telus-csd-tvs/opus-sdm-newrelic/actions).",
-    ].join("\n"),
-  });
-  await github.rest.issues.addLabels({
-    owner,
-    repo,
-    issue_number: data.number,
-    labels: ["feature", "automated pr"],
-  });
-  await github.rest.pulls.requestReviewers({
-    owner,
-    repo,
-    pull_number: data.number,
-    reviewers: ["tamy-vivas"],
-  });
+module.exports = async ({ github, context, core }) => {
+  try {
+    const { repo, owner } = context.repo;
+    const { data } = await github.rest.pulls.create({
+      title: "[NEW PR] Sync main",
+      owner,
+      repo,
+      head: "sit",
+      base: "main",
+      body: [
+        "This PR is auto-generated",
+        "[Full history of actions](https://github.com/telus-csd-tvs/opus-sdm-newrelic/actions).",
+      ].join("\n"),
+    });
+    await github.rest.issues.addLabels({
+      owner,
+      repo,
+      issue_number: data.number,
+      labels: ["feature", "automated pr"],
+    });
+    await github.rest.pulls.requestReviewers({
+      owner,
+      repo,
+      pull_number: data.number,
+      reviewers: ["tamy-vivas"],
+    });
 
-  return data;
+    core.notice(`PR created: ${data.number}`);
+  } catch (error) {
+    throw new Error(error);
+  }
 };
