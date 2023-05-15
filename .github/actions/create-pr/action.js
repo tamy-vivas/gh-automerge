4ba8ae2210053b23754d8a30ad7b060aaf9767ee
tamy-vivas <tamy.vivas@telusinternational.com>
2023-05-15 10:58:43 -0600

    test: 1

---
+++ .github/actions/create-pr/action.js
@@ -0,0 +1,28 @@
+module.exports = async ({ github, context }) => {
+  const { repo, owner } = context.repo;
+  const { data } = await github.rest.pulls.create({
+    title: "[NEW PR] Sync main",
+    owner,
+    repo,
+    head: "sit",
+    base: "main",
+    body: [
+      "This PR is auto-generated",
+      "[Full history of actions](https://github.com/telus-csd-tvs/opus-sdm-newrelic/actions).",
+    ].join("\n"),
+  });
+  await github.rest.issues.addLabels({
+    owner,
+    repo,
+    issue_number: data.number,
+    labels: ["feature", "automated pr"],
+  });
+  await github.rest.pulls.requestReviewers({
+    owner,
+    repo,
+    pull_number: data.number,
+    reviewers: ["tamy-vivas"],
+  });
+
+  return data;
+};
